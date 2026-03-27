c379962ba77364b88ef057a295b3b5c6cfc875d9
Sergey Badin <sergeykfg1@me.com>
2026-03-27 12:47:26 +0100

    refactor: neu users and update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import connectToDatabase from "@/lib/db/connectToDatabase";
+import User from "@/lib/db/models/User";
 
 export const authOptions = {
   providers: [
@@ -9,6 +11,26 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user }) {
+      if (!user?.email || !user?.name) return false;
+
+      await connectToDatabase();
+      const existingUser = await User.findOne({ googleId: user.email });
+
+      if (!existingUser) {
+        await User.create({
+          googleId: user.email,
+          name: user.name,
+          email: user.email,
+          image: user.image || "",
+          nickname: user.name,
+        });
+      }
+
+      return true;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
